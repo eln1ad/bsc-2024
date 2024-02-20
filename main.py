c8460279d74359a81bffc64911c79dc76bc85726
eln1ad <semseyd2000@gmail.com>
2024-02-20 19:35:28 +0100

    added NMS algorithm to filter predictions

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from action_classification.generator import load_label_list
 
 
 # 1. Get a video
-# 2. Break to video into sliding windows
-# 3. Extract features with feature extractor (length=8, stride=1)
-# 4. Input the features into the detector model to get predictions (action/background, delta_center, delta_length)
-# 5. Continue with the action proposals only, refine their boundaries with delta_center, delta_length
-# 6. Get the features of the refined window and input them into the classifier to predict the class
-# 7. Filter proposals with NMS
+# 2. Get sliding windows for the video
+# 3. Extract features for the whole video, with a fixed range (length=8, stride=1)
+# 4. Input the features corresponding to a sliding window to the action detector model to get stage 1 predictions (action/background, delta_center, delta_length)
+# 5. Continue with those sliding windows that are actions
+# 6. Calculate the new start and end coordinates of those windows, using delta_center and delta_length
+# 7. Using this new regressed window load in the necessary features
+# 8. Input features to the action classifier model to predict the class of the action
+# 9. Filter the output windows with the NMS algorithm
+# 10. Display the information of the retained windows (start, end, label, confidence)
 
 
 def get_rgb_frames(path: str, frame_size):
@@ -42,44 +45,54 @@ def get_window_intervals(num_frames, sizes = [8, 10, 12, 14, 16], overlap = 0.8)
     return windows
 
 
+def NMS(segments, confidences, tiou_threshold=0.2):
+    starts = segments[:, 0]
+    ends = segments[:, 1]
+    
+    # sort the scores by indices descending (highest first)
+    indices = np.argsort(confidences)[::-1]
+    areas = (ends - starts).astype(float)
 
-# def get_window_indices(sample_count, start, end):
-#     n = end - start
-#     sample_rate = int(n / sample_count)
-#     indices = np.arange(start, end)
+    filtered = []
     
-#     out_indices = []
-    
-#     if sample_rate >= 2:
-#         for i in range(sample_count):
-#             group = np.arange(i * sample_rate, (i + 1) * sample_rate)
-#             i = np.random.choice(group, 1, replace=False).item()
-#             out_indices.append(indices[i])
-#         if (n - sample_count * sample_rate) > 0:
-#             left_overs = np.arange(sample_count * sample_rate, n)
-#             i = np.random.choice(left_overs, 1, replace=False).item()
-#             out_indices.append(indices[i])
-#             out_indices = np.sort(np.random.choice(out_indices, sample_count, replace=False))
-#     else:
-#         if n < sample_count:
-#             out_indices = np.sort(np.random.choice(indices, sample_count, replace=True))
-#         else:
-#             out_indices = np.sort(np.random.choice(indices, sample_count, replace=False))
-            
-#     return out_indices
-    
-    
+    while len(indices) > 0:
+        best_idx = indices[0]
+        filtered.append(best_idx)
+        
+        indices = indices[1:] # delete first (best) item
 
+        # get intersection between the best box and the rest
+        intersection_starts = np.maximum(starts[best_idx], starts[indices])
+        intersection_ends = np.minimum(ends[best_idx], ends[indices])
+
+        # calculate tiou
+        intersections = np.maximum(0., intersection_ends - intersection_starts)
+        unions = areas[best_idx] + areas[indices] - intersections
+        tious = intersections / unions
+
+        # remove those segments that have high tiou with best
+        indices = indices[np.nonzero(tious <= tiou_threshold)[0]]
+
+    # return indices of filtered segments
+    return filtered
+    
 
 if __name__ == "__main__":
-    video_name = "video_000351.mp4"
-    frame_size = (112, 112)
-    c3d_capacity = 8
-    modality = "rgb"
-    
     configs_dir = Path.cwd().joinpath("configs")
-    saved_models_dir = Path.cwd().joinpath("saved_models")
+    c3d_config = load_json(configs_dir.joinpath("C3D.json"))
     paths_config = load_json(configs_dir.joinpath("paths.json"))
+    action_detector_config = load_json(configs_dir.joinpath("action_detector.json"))
+    action_classifier_config = load_json(configs_dir.joinpath("action_classifier.json"))
+    
+    
+    video_name = "video_000351.mp4"
+    frame_size = (c3d_config["image_width"], c3d_config["image_height"])
+    capacity = c3d_config["capacity"]
+    modality = c3d_config["modality"]
+    nms_threshold = 0.2
+    
+    
+    saved_models_dir = Path.cwd().joinpath("saved_models")
     video_path = Path(paths_config["videos_dir"]).joinpath(video_name)
     
     frames = get_rgb_frames(str(video_path), frame_size)
@@ -93,7 +106,9 @@ if __name__ == "__main__":
     action_classifier_name = f"action_classifier_{modality}_50_epochs"
     action_classifier = keras.models.load_model(saved_models_dir.joinpath(action_classifier_name))
     
-    window_size = 8
+    
+    # window_size used for extracting features
+    window_size = c3d_config["capacity"]
     window_stride = 1
     
     features_dict = {}
@@ -110,9 +125,10 @@ if __name__ == "__main__":
         
         features_dict[f"{start}_{end}"] = features
         
-    # Create sliding windows
+    window_sizes = [8, 10, 12, 14, 16]
+    window_intervals = get_window_intervals(frames.shape[0], sizes=window_sizes, overlap=0.8)
     
-    window_intervals = get_window_intervals(frames.shape[0], sizes=[8, 10, 12, 14, 16], overlap=0.8)
+    predictions = []
     
     for window_interval in window_intervals:
         # Load in corresponding features
@@ -145,20 +161,31 @@ if __name__ == "__main__":
             pred_end = np.round(pred_center + pred_length / 2.0)
             pred_end = np.clip(pred_end, a_min=0, a_max=window_interval[1])
             
-            regressed_window_features = []
+            window_features = []
             
             for k in features_dict.keys():
                 parts = k.split("_")
                 start, end = int(parts[0]), int(parts[1])
                 if pred_start <= start and end <= pred_end:
-                    regressed_window_features.append(features_dict[k])
+                    window_features.append(features_dict[k])
             
-            regressed_window_features = np.average(regressed_window_features, axis=0)
-            regressed_window_features = np.expand_dims(regressed_window_features, axis=0)
+            window_features = np.average(window_features, axis=0)
+            window_features = np.expand_dims(window_features, axis=0)
             
-            pred = action_classifier(regressed_window_features)[0]
+            pred = action_classifier(window_features)[0]
             pred_confidence = round(np.max(pred), 4)
             pred_label = label_list[np.argmax(pred)]
             
-            print(f">>> PREDICTION FOR {Path(video_name).stem} <<<")
-            print(f"label: {pred_label}, confidence: {pred_confidence}, start: {pred_start}, end: {pred_end}\n")
+            predictions.append([pred_start, pred_end, pred_confidence, pred_label])
+            
+    predictions = np.array(predictions)
+    # casting is needed, because there is a string column inside predictions
+    indices = NMS(predictions[:, :2].astype(float), predictions[:, 2].astype(float), tiou_threshold=nms_threshold)
+    predictions = predictions[indices]
+    
+    for prediction in predictions:
+        pred_label = prediction[3]
+        pred_start, pred_end = prediction[0], prediction[1]
+        pred_confidence = prediction[2]
+        print(f">>> PREDICTION FOR {Path(video_name).stem} <<<")
+        print(f"label: {pred_label}, confidence: {pred_confidence}, start: {pred_start}, end: {pred_end}\n")
